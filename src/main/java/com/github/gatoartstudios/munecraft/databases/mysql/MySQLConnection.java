1ef97814f514cd9f40d6566e891057c659004ca4
gatoartstudio <hackhcctecno@gmail.com>
2025-05-06 14:10:17 -0500

    Add: A method was added, which will run in a separate thread, to keep the connection with the database alive. This method will execute a query every 5 minutes on the database.

--- src/main/java/com/github/gatoartstudios/munecraft/databases/mysql/MySQLConnection.java
+++ src/main/java/com/github/gatoartstudios/munecraft/databases/mysql/MySQLConnection.java
@@ -4,12 +4,18 @@ import com.github.gatoartstudios.munecraft.core.event.EventDispatcher;
 import com.github.gatoartstudios.munecraft.models.SQL;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MySQLConnection {
     private final String URL;
     private final SQL sql;
 
     private final Connection connection;
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
 
     /**
      * Creates a connection to a MySQL database.
@@ -22,6 +28,7 @@ public class MySQLConnection {
         URL = "jdbc:mysql://" + sql.getHost() + ":" + sql.getPort() + "/" + sql.getDatabase();
         try {
             this.connection = java.sql.DriverManager.getConnection(URL, sql.getUser(), sql.getPassword());
+            startKeepAliveTask();
         } catch (Exception e) {
             EventDispatcher.dispatchAlert(e.getMessage());
             throw new RuntimeException(e);
@@ -35,5 +42,21 @@ public class MySQLConnection {
      */
     public Connection getConnection() {
         return connection;
+    }
+
+    /**
+     * Creates a looping task in another thread, which keeps the connection
+     * to the database active by performing a query every 5 minutes.
+     */
+    private void startKeepAliveTask() {
+        Runnable keepAlive = () -> {
+            try (Statement stmt = connection.createStatement()) {
+                stmt.execute("SELECT 1");
+            } catch (SQLException e) {
+                EventDispatcher.dispatchAlert("Error in the connection maintenance task: " + e.getMessage());
+            }
+        };
+
+        scheduledExecutorService.scheduleAtFixedRate(keepAlive, 5, 5, TimeUnit.MINUTES);
     }
 }
